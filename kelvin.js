e9ae8fcb2f6cae3b446856340c6389bdfec2d219
jkmunoz <juliannakmunoz@gmail.com>
2023-08-03 13:26:22 -0500

    project complete

--- kelvin.js
+++ kelvin.js
@@ -7,10 +7,13 @@ const celsius = kelvin - 273;
 // I am using var in this instance because we will be rounding down to get fahrenheit, meaning the number will need to change.
 var fahrenheit = celsius * (9/5) + 32;
 
-// This should take the temp in F and roudn the decimal number down. Since it comes after our orginal 'fahrenheit' definition
-// it should be what prints in the console. 
-fahrenheit = Math.floor();
+// This should take the temp in F and roudn the decimal number down.
+Math.floor(fahrenheit);
 
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
 
 
+// Open the related index.html doc and use the dev tools to see the codes results. 
+// Simply change the value of 'kelvin' to get a new temp. 
+
+// For example, 0 K is -459.40000000000003 degrees Fahrenheit. 
